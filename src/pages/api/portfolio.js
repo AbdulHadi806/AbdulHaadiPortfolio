a1f7dcc1f3c93ce74dbb857e8f198d294e4f844a
Abdul Hadi <abdul.806@yahoo.com>
2023-02-18 01:56:38 +0500

    SS

--- src/pages/api/portfolio.js
+++ src/pages/api/portfolio.js
@@ -3,7 +3,7 @@ import { promises as fs } from 'fs';
 
 export default async function handler(req, res) {
     try {
-        const jsonDirectory = path.join(process.cwd(), 'src/json/static.json');
+        const jsonDirectory = path.join(process.cwd(), '/src/pages/json/static.json');
         const fileContents = await fs.readFile(jsonDirectory,'utf8' );
         res.status(200).json(JSON.parse(fileContents));
     } catch (error) {
