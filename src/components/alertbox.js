3150a50aa172068a1f137b67be6f9d365956cc23
Abdul Hadi <abdul.806@yahoo.com>
2023-02-23 21:55:35 +0500

    ss

--- src/components/alertbox.js
+++ src/components/alertbox.js
@@ -5,3 +5,4 @@ export const Alertbox1 = ({ toggleHandler, toggle, message }) => {
 export const Alertbox2 = ({ toggleHandler, message, toggle }) => {
     return (<div className={toggle ? "d-none" : "alert alert-danger alert-dismissible fade show"} data-bs-dismiss="alert" role="alert" > <strong>Opps!</strong>{' '} {message} <button onClick={toggleHandler} type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>)
 }
+ 
